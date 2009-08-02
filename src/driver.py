ab4a74a41511ad9e2370e25f88b7abf7c2f2fa44
alex <alex@alex-desktop>
2009-08-02 17:33:36 -0400

    Fixed numerous reproduction issues, still overgrows though.

--- src/driver.py
+++ src/driver.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
-import pygame, sys
+import sys
+
+import pygame
 from pygame.locals import *
-from state import *
+from state import State
 
 state = State()
 pygame.init()
@@ -12,14 +14,20 @@ pygame.display.set_caption("Cauto")
 clock = pygame.time.Clock()
 
 while True:
-	clock.tick(10)
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit(0) 
-##		else: 
-##			print event
-	for cell in state.cells:
-		print state.time, cell.position, cell.radius
-		pygame.draw.circle(screen, pygame.Color("red"), cell.position, cell.radius)
-	state.next()
-	pygame.display.flip()
+    # Limits simulation frame rate
+    clock.tick(10)
+
+    # Exit on window close
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+#		else:
+#			print event
+
+    screen.fill(pygame.Color("white"))
+    for cell in state.cells:
+        pygame.draw.circle(screen, pygame.Color("red"), cell.position, cell.radius)
+        pygame.draw.circle(screen, pygame.Color("black"), cell.position, cell.radius, 1)
+    pygame.display.flip()
+    state.next()
--- src/state.py
+++ src/state.py
@@ -1,89 +1,86 @@
 #!/usr/bin/env python
 import math
-import random
 
 from cell import Cell
+import random
 
 class State:
 
-	def __init__(self, AGE=10, dimensions=(200, 200)):
-		self.AGE = AGE
-		self.dimensions = dimensions
-		self.cells = []
-		self.time = 0
-		# Start off with a cell in each corner
-		self.cells.append(Cell((11, 11)))
-		self.cells.append(Cell((11, dimensions[1]-11)))
-		self.cells.append(Cell((dimensions[0]-11, 11)))
-		self.cells.append(Cell((dimensions[0]-11, dimensions[1]-11)))
+    def __init__(self, AGE=10, dimensions=(200, 200)):
+        self.AGE = AGE
+        self.dimensions = dimensions
+        self.cells = []
+        self.time = 0
+        # Start off with a cell in each corner
+        self.cells.append(Cell((10, 10)))
+        self.cells.append(Cell((10, dimensions[1]-10)))
+        self.cells.append(Cell((dimensions[0]-10, 10)))
+        self.cells.append(Cell((dimensions[0]-10, dimensions[1]-10)))
 
-	def next(self, speed=1):
-		# Cells older than AGE reproduce
-		for x in range(len(self.cells)):
-			try:
-				if self.cells[x].age == self.AGE:
-					if self.cells[x].health >= 50:
-						# First daughter, tangent to current
-						newCell = Cell(self.cells[x].position, self.cells[x].dna.mutate())
-						for theta in range(0, 200 * math.pi, 200 * math.pi / 18):
-							newCell.position = (self.cells[x].position[0] + self.cells[x].radius * math.cos(theta / 100), \
-												self.cells[x].position[1] + self.cells[x].radius * math.sin(theta / 100))
-							if not self.checkCollision(newCell, self.cells[x]):
-								self.cells.append(newCell)
-								break
-						# Second daughter, in same place as current
-						self.cells.append(Cell(self.cells[x].position, self.cells[x].dna.mutate()))
-						# Kill current cell
-						self.cells[x].health = 0
-					else:
-						self.cells[x].health = 0
-				else:
-					self.cells[x].age += 1
-			except:
-				pass
+    def next(self, speed=1):
+        # Cells older than AGE reproduce
+        for cell in self.cells:
+            if cell.age >= self.AGE:
+                if cell.health >= 50:
+                    # Add two tangent daughter cells
+                    for i in range(2):
+                        newCell = Cell(cell.position, cell.dna.mutate())
+                        start = random.uniform(0, 200 * math.pi)
+                        for theta in range(start, start + 200 * math.pi, 200 * math.pi / 18):
+                            newCell.position = (cell.position[0] + cell.radius * 2 * math.cos(theta / 100), \
+                                                cell.position[1] + cell.radius * 2 * math.sin(theta / 100))
+                            if not self.check_collision(newCell):
+                                self.cells.append(newCell)
+                                break
+                    # Second daughter, in same place as current
+#                    self.cells.append(Cell(cell.position, cell.dna.mutate()))
+    # Kill current cell
+    cell.health = 0
+else:
+    cell.health = 0
+else:
+    cell.age += 1
 
-		# Remove dead cells
-		for y in range(len(self.cells)):
-			try:
-				if (self.cells[y].health <= 0):
-					self.cells.pop(y)
-				else:
-					pass
-			except:
-				pass
-		
-		self.time += 1
+# Remove dead cells
+    for y in range(len(self.cells)):
+        try:
+            if (self.cells[y].health <= 0):
+                self.cells.pop(y)
+        except IndexError:
+            pass
 
-	def __str__(self):
-		return_str = "Cells:"
+    self.time += 1
 
-		for x in self.cells:
-			return_str += "\n---\n"
-			return_str += str(x)
+def __str__(self):
+    return_str = "Cells:"
 
-		return return_str
+    for x in self.cells:
+        return_str += "\n---\n"
+        return_str += str(x)
 
-	def add_cell(self, cell=Cell((0, 0))):
-		self.cells.append(cell)
+    return return_str
 
-	def pop_first_cell(self):
-		return self.cells.pop()
+def add_cell(self, cell=Cell((0, 0))):
+    self.cells.append(cell)
 
-	def checkCollision(self, cell1):
-		# Check boundary collisions
-		if (cell1.position[0]-cell1.radius < 0) or (cell1.position[0] + cell1.radius > self.dimensions[0]) \
-		or (cell1.position[1]-cell1.radius < 0) or (cell1.position[1] + cell1.radius > self.dimensions[1]):
-			return True
-		# Check cell collisions
-		for x in xrange(self.cells):
-			if self.distanceSquared(self.cells[x], cell1) < (cell1.radius ** 2 + self.cells[x].radius ** 2):
-				return True
-			return False
+def pop_first_cell(self):
+    return self.cells.pop()
 
-	def distanceSquared(self, cell1, cell2):
-		'''Returns the distance between two cells'''
-		dist = (cell1.position[0] - cell2.position[0]) ** 2 + (cell1.position[1] - cell2.position[1]) ** 2
-		if dist > 1:
-			return dist
-		else:
-			return 1
+def check_collision(self, cell1):
+    # Check boundary collisions
+    if (cell1.position[0]-cell1.radius < 0) or (cell1.position[0] + cell1.radius > self.dimensions[0]) \
+    or (cell1.position[1]-cell1.radius < 0) or (cell1.position[1] + cell1.radius > self.dimensions[1]):
+        return True
+    # Check cell collisions
+    for cell2 in self.cells:
+        if self.distance_squared(cell2, cell1) < (cell1.radius + cell2.radius) ** 2:
+            return True
+        return False
+
+def distance_squared(self, cell1, cell2):
+    '''Returns the distance between two cells'''
+    dist = (cell1.position[0] - cell2.position[0]) ** 2 + (cell1.position[1] - cell2.position[1]) ** 2
+    if dist > 1:
+        return dist
+    else:
+        return 1
