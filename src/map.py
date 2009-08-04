f17f2266152de5cef0abcec426061bc402daf067
samir <samir@curtisjr.curtiscluster>
2009-08-03 21:38:55 -0400

    Added basic contents to map.

--- src/map.py
+++ src/map.py
@@ -1,6 +1,10 @@
 #/usr/bin/python
 
-class map:
+class Map:
 
-    def __init__(x,y):
-	self.grid = range(x)
+    def __init__(self,x,y):
+	self.grid = range(y)
+	for z in range(len(self.grid)):
+	    self.grid[z]=range(x)
+	    for a in range(len(self.grid[z])):
+		self.grid[z][a] = 0
