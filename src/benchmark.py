c2c32a5a344a315ccb198e74f99bcc5c45eaae2a
alex <alex@alex-desktop>
2009-08-04 21:43:12 -0400

    Added a basic benchmark module. Minor optimizations.

--- src/benchmark.py
+++ src/benchmark.py
@@ -1,5 +1,7 @@
+import os.path
 #!/usr/bin/env python
 import sys
+import os
 import pygame
 from pygame.locals import *
 from state import State
@@ -8,7 +10,7 @@ from cell import Cell
 state = State(dimensions=(500,500))
 pygame.init()
 screen = pygame.display.set_mode(state.dimensions)
-pygame.display.set_caption("Cauto")
+pygame.display.set_caption("Cauto Bench")
 #screen = pygame.display.get_surface().convert()
 
 clock = pygame.time.Clock()
@@ -32,20 +34,23 @@ def handle_events():
             print "Simulating at %s fps" % rate
         elif event.type == MOUSEBUTTONDOWN:
              state.cells.append(Cell(event.pos))
-             
+
 def exit():
     pygame.quit()
     sys.exit(0)
 
-while True:
+fps = []
+for i in xrange(500):
     # Limits simulation frame rate
-    clock.tick(rate)
-
+    clock.tick()
+    if i > 0 and i % 10 == 0:
+        fps.append(str(clock.get_fps())+"\n")
     # Handle events
     handle_events()
     while rate==0:
         handle_events()
 
+#    screen.fill(pygame.Color("white"))
     # First, draw map
     for a  in xrange(len(state.themap.grid)):
 	for b in xrange(len(state.themap.grid[a])):
@@ -55,6 +60,9 @@ while True:
     for cell in state.cells:
         pygame.draw.circle(screen, (255-cell.age*10,255-cell.age*10,255-cell.age*10), cell.position, cell.radius)
         pygame.draw.circle(screen, pygame.Color(int(cell.health*2),0,0), cell.position, cell.radius, 2)
-    
+
     pygame.display.flip()
     state.next()
+
+with open(os.path.expanduser("~/Desktop/bench.txt"), "w") as f:
+    f.writelines(fps)
