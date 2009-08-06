fa88b66c1d7128ad810f0b1e7adb3c46cd1035d9
AC <accurrent@gmail.com>
2009-08-06 17:48:45 -0400

    Added all antibiotic disc function. Changed color pallete.

--- src/driver.py
+++ src/driver.py
@@ -4,6 +4,7 @@ import pygame
 from pygame.locals import *
 from state import State
 from cell import Cell
+from disc import Disc
 
 state = State(dimensions=(500,500))
 pygame.init()
@@ -20,7 +21,7 @@ def handle_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
-        elif event.type == KEYUP:
+        elif event.type == KEYDOWN:
             if event.key == 275:
                 rate += 5
             elif event.key == 276:
@@ -31,12 +32,15 @@ def handle_events():
                 exit()
             print "Simulating at %s fps" % rate
         elif event.type == MOUSEBUTTONDOWN:
-             state.cells.append(Cell(event.pos))
-             cell = state.cells[-1]
-             # Draw the cell immediately
-             pygame.draw.circle(screen, (cell.dna.toxin_type*75,cell.dna.toxin_strength,255), cell.position, cell.radius)
-             pygame.display.update(pygame.draw.circle(screen, (cell.dna.wall_type*75,cell.dna.wall_width*255,255), cell.position, cell.radius, 4))
-             
+            if event.button == 1:
+                state.cells.append(Cell(event.pos))
+                cell = state.cells[-1]
+                # Draw the cell immediately
+                pygame.draw.circle(screen, (255-(cell.age*10),255-(cell.age*10),255-(cell.age*10)), cell.position, cell.radius)
+                pygame.display.update(pygame.draw.circle(screen, (cell.health *2.5, cell.health *2.5, cell.health *2.5), cell.position, cell.radius, 4))
+            elif event.button == 3:
+                state.discs.append(Disc(event.pos))
+                disc = state.discs[-1]
              
 def exit():
     pygame.quit()
@@ -52,16 +56,24 @@ while True:
         handle_events()
 
     # First, draw map
-    screen.fill((100, 245, 100))
+    screen.fill((50, 195, 50))
     for a in xrange(len(state.themap.grid)):
         for b in xrange(len(state.themap.grid[a])):
             if state.themap.grid[b][a][0] != 1:
-                screen.fill((100,100+state.themap.grid[b][a][0]*145,100),pygame.Rect(a*state.themap.cell_width,b*state.themap.cell_height,state.themap.cell_width,state.themap.cell_height))
+                screen.fill((50,50+state.themap.grid[b][a][0]*145,50),pygame.Rect(a*state.themap.cell_width,b*state.themap.cell_height,state.themap.cell_width,state.themap.cell_height))
+    
+    # Second, draw antibiotic discs
+    for disc in state.discs:
+        color = [0, pygame.Color("red"), pygame.Color("orange"), pygame.Color("yellow")][disc.type]
+        pygame.draw.circle(screen, color, disc.position, disc.radius)
 
-    # Second, draw cells
+    # Third, draw cells
     for cell in state.cells:
-        pygame.draw.circle(screen, (255-(cell.age*10),255-(cell.age*10),255-(cell.age*10)), cell.position, cell.radius)
-        pygame.draw.circle(screen, (cell.health *2.5, cell.health *2.5, cell.health *2.5), cell.position, cell.radius, 4)
+        body = (255-(cell.age*10),255-(cell.age*10),255-(cell.age*10))
+        pygame.draw.circle(screen, body, cell.position, cell.radius)
+        
+        border = [pygame.Color("black"), pygame.Color("red"), pygame.Color("orange"), pygame.Color("yellow")][cell.dna.wall_type]
+        pygame.draw.circle(screen, border, cell.position, cell.radius, 4)
     
     pygame.display.update()
     state.next()
